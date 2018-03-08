0a5567c618087fdfa1c00f689a0208329512d294
Alon Levy <alon@pobox.com>
2018-03-08 11:59:57 +0200

    get_appveyor_last_artifact: use wget instead of requests (target platform already has, reduces need for pip)

--- util/get_appveyor_last_artifact.py
+++ util/get_appveyor_last_artifact.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
-from requests import get
 from os import system, path
+import json
 import os
 
-
-response = get('https://ci.appveyor.com/api/projects/alon/pump-summarize')
-js = response.json()
+os.system('wget -O pump-summarize.json https://ci.appveyor.com/api/projects/alon/pump-summarize')
+with open('pump-summarize.json') as fd:
+    js = json.load(fd)
 last_job = js['build']['jobs'][0]
-job_id = ['jobId']
+job_id = last_job['jobId']
+job_updated = last_job['updated']
 print('job_id = {}'.format(job_id))
-print('job updated: {}'.format(last_job['updated']))
+print('job updated: {}'.format(job_updated))
 summarize_url = 'https://ci.appveyor.com/api/buildjobs/{}/artifacts/dist%2Fsummarize.zip'.format(job_id)
 with open('index.html', 'w') as fd:
-    fd.write("""<!DOCTYPE html><html><head><title>Summarize zip file</title></head><body><a href="{}">summarize.zip</a></body>""".format(summarize_url))
+    fd.write("""<!DOCTYPE html><html><head><title>Summarize zip file (job_id {}, created {})</title></head><body><a href="{}">summarize.zip</a></body>""".format(
+        job_id, job_updated, summarize_url))
 if path.exists('summarize.zip'):
     print("removing existing summarize.zip file")
     os.unlink('summarize.zip')
